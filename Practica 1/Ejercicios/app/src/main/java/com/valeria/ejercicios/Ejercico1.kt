149378bd99fb9a0f9e9acf8d55810f98e9367527
Valercat <00002024@uca.edu.sv>
2026-03-22 00:08:13 -0600

    Ejercicios practica 1

---
+++ Practica 1/Ejercicios/app/src/main/java/com/valeria/ejercicios/Ejercico1.kt
@@ -0,0 +1,51 @@
+package com.valeria.ejercicios
+
+//1.
+//fun main() {
+//    println("1")
+//    println("2")
+//    println("3")
+//}
+//R:
+// 1
+// 2
+// 3
+
+//2.
+//fun main(){
+//    println("I'm")
+//    println("learning")
+//    println("Kotlin!")
+//}
+
+//3.
+//fun main() {
+//    println("Monday")
+//    println("Tuesday")
+//    println("Wednesday")
+//    println("Thursday")
+//    println("Friday")
+//}
+
+//4.
+//fun main() {
+//    println("Tomorrow is rainy")
+//}
+
+//5.
+//fun main() {
+//    println("There is a chance of snow")
+//}
+
+//6.
+//fun main() {
+//    println("Cloudy")
+//    println("Partly Cloudy")
+//    println("Windy")
+//}
+
+//7.
+//fun main() {
+//    println("How's the weather today?")
+//}
+
